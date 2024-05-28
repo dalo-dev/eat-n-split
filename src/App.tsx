2ded770548cf81fc2940d7fb24b74c7f81031f3f
Daniel Leon <dalodev@proton.me>
2024-05-28 10:27:01 -0500

    Added friend list

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import FriendsList from "./FriendsList";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
